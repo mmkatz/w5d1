f3cd752e07f6aba990c8cb9b7689db819ac14375
mmk <menachem3093@gmail.com>
2021-01-25 17:24:17 -0500

    phase 3

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,34 @@ class HashSet
   end
 
   def insert(key)
+      if !self.include?(key)
+        self[key.hash] << key 
+        @count += 1 
+
+      end 
+      resize! if @count > num_buckets
   end
 
   def include?(key)
+     self[key.hash].each do |number|
+      return true if number == key
+    end
+    return false
   end
 
   def remove(key)
+      if self.include?(key)
+      self[key.hash].delete(key) 
+         @count -= 1 
+      end 
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+      mod = num % num_buckets
+    return @store[mod]
   end
 
   def num_buckets
@@ -26,5 +42,13 @@ class HashSet
   end
 
   def resize!
+      new_store = Array.new(num_buckets * 2 ) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |ele|
+        new_store[ele.hash % (num_buckets * 2)] << ele 
+      end
+    end
+    @store = new_store 
   end
+  
 end
