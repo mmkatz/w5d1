f0230425daf7623aec8241fa65862ad782f746f4
Lane Harris <lrharris215@gmail.com>
2021-01-25 16:47:17 -0500

    String#hash and Hash#hash

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -14,6 +14,11 @@ end
 
 class String
   def hash
+    hash_key = 531
+    self.each_char.with_index do |char, i|
+      hash_key += (char.ord.hash ^ i.hash)
+    end
+    hash_key
   end
 end
 
@@ -21,6 +26,17 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = []
+    self.each do |k, v| 
+      arr << [k,v]
+    end
+    arr.sort!
+    hash_key = 934
+    arr.each do |sub|
+      sub.each_with_index do |ele, idx|
+        hash_key += (ele.hash ^ idx.hash)
+      end
+    end
+    hash_key
   end
 end
