1f9a8b46fad5767b5812c07324b50c6224f8f2ec
mmk <menachem3093@gmail.com>
2021-01-25 15:18:26 -0500

    completed resize

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -81,21 +81,37 @@ class ResizingIntSet
   end
 
   def insert(num)
-      
+    if !self.include?(num)
+        self[num] << num 
+        @count += 1 
+    end
+
+    if @count > num_buckets
+      resize! 
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num) 
+         @count -= 1 
+    end
+ 
   end
 
   def include?(num)
-    
+      self[num].each do |number|
+      return true if number == num
+    end
+    return false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
-    
+    mod = num % num_buckets
+    return @store[mod]
   end
 
   def num_buckets
@@ -103,5 +119,12 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2 ) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |ele|
+        new_store[ele % (num_buckets * 2)] << ele 
+      end
+    end
+    @store = new_store 
   end
 end
